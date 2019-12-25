aa49a044eca0f87b224d53a476bb32ee4dec2225
Tim <tian.xie@china.zhaogang.com>
2019-12-25 13:53:25 +0800

    ✨:Introducing new features for parrot.

---
+++ parrot.py
@@ -0,0 +1,14 @@
+
+# 鹦鹉学舌
+active = True
+while active:
+    parrot = '鹦鹉: 主人，跟我说一句话，然后我学给你听！'
+    master = '\n主人: '
+    message = input(parrot + master)
+    print('鹦鹉: ' + message + '~')
+
+    quit_order = input('鹦鹉: 主人，还玩不玩了' + '\n(玩/不玩)')
+    if (quit_order != '不玩') & (quit_order != '不玩了'):
+        active = True
+    else:
+        active = False
