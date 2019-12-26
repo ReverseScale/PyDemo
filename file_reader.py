7eaba126529f8fed4dbf55d6ac7d3b02caff11ae
Tim <tian.xie@china.zhaogang.com>
2019-12-26 17:49:17 +0800

    ✨:Introducing new features for r and w.

---
+++ file_reader.py
@@ -0,0 +1,13 @@
+filename = 'text_files/pi_digits.txt'
+with open(filename) as file_object:
+    # contents = file_object.read()
+    # print(contents)
+    
+    lines = file_object.readlines()
+    pi_string = ''
+
+    for line in lines:
+        pi_string += line.strip()
+
+        print(pi_string[:52] + '...')
+        print(len(pi_string))
---
+++ write_message.py
@@ -0,0 +1,7 @@
+filename = 'text_files/programming.txt'
+
+# with open(filename, 'w') as file_object:
+#     file_object.write('hello python.')
+
+with open(filename, 'a') as file_object:
+    file_object.write('hello human.')
